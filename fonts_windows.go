abed0e799c4fe31e44f408c98fe4b1d4be7faf5d
Jeremy Price <donomii@gmail.com>
2019-04-12 00:15:43 +0200

    Fix for windows

--- fonts_windows.go
+++ fonts_windows.go
@@ -2,33 +2,26 @@
 package glim
 
 import (
-	_ "golang.org/x/image/font/gofont/goregular"
 	"golang.org/x/image/font/gofont/gomono"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"os"
+	_ "golang.org/x/image/font/gofont/goregular"
 
-	"github.com/kardianos/osext"
-	
 	"github.com/golang/freetype/truetype"
 )
 
- //Attempts to load a font using goMobile's truetype font library
- func LoadFont(fileName string) *truetype.Font {
+//Attempts to load a font using goMobile's truetype font library
+func LoadFont(fileName string) *truetype.Font {
 
-     if fontCache == nil {
-         fontCache = map[string]*truetype.Font{}
-     }
-     im, ok := fontCache[fileName]
-     if ok {
-         return im
-     }
+	if fontCache == nil {
+		fontCache = map[string]*truetype.Font{}
+	}
+	im, ok := fontCache[fileName]
+	if ok {
+		return im
+	}
 
-     //fontBytes := sysFont.Default()
+	//fontBytes := sysFont.Default()
 
-     txtFont, _ := truetype.Parse(gomono.TTF)
-     fontCache[fileName] = txtFont
-     return txtFont
- } 
+	txtFont, _ := truetype.Parse(gomono.TTF)
+	fontCache[fileName] = txtFont
+	return txtFont
+}
